324eddf4f812c3a6a496e1d908d67f4cc0039aa0
matamor <matamor98@hotmail.com>
2021-11-29 22:40:50 +0100

    Initial commit

---
+++ app/src/main/java/me/matamor/desarrollointerfaces/MainActivity.java
@@ -0,0 +1,22 @@
+package me.matamor.desarrollointerfaces;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
